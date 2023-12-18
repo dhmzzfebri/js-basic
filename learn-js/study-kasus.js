7cf37deffd4baeacd69f2d47eb3eaaaabc64aadc
biodata <dimasfebriyanto725@gmail.com>
2023-12-18 10:51:20 +0700

    Feat:conditional in js

---
+++ learn-js/study-kasus.js
@@ -0,0 +1,10 @@
+const value = 10;
+let answer;
+
+if (value < 0){
+    answer ="ini bilangan posotif";
+}else if(value <0){
+    answer ="ini bilangan negatif";
+}else{
+    answer = 'bukan bilangan';
+}
---
+++ learn-js/switch.js
@@ -0,0 +1,10 @@
+switch("selasa"){
+    case"senin":
+        console.log("ini hari senin");
+        break;
+    case"selasa":
+        console.log("ini hari selasa");
+        break;
+    default:
+        console.log("ini bukan hari senin atau selasa");
+}
