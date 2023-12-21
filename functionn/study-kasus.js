ea281fb31c974256cbddf98561a44b69c081f162
biodata <dimasfebriyanto725@gmail.com>
2023-12-21 14:56:28 +0700

    update file function

---
+++ functionn/study-kasus.js
@@ -0,0 +1,28 @@
+function greetingProgram(name){
+    return "halo dek ," + name + " apakabarnya?"
+}
+console.log(greetingProgram("dila"));
+
+
+// buat fungsi utk menambah bilangan
+function add1(bil1,bil2){
+    return bil1+bil2;
+}
+console.log(add1(10,5));
+
+
+// buat fungsi pengurangan,perkalian dan pembagian
+function add2(bil1,bil2){
+    return bil1-bil2;
+}
+console.log(add2(10,5));
+
+function add3(bil1,bil2){
+    return bil1*bil2;
+}
+console.log(add3(5,2));
+
+function add4(bil1,bil2){
+    return bil1/bil2;
+}
+console.log(add4(30,10));
