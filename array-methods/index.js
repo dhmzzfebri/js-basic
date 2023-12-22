5578bd20269f16a312fe1630e48947e6b12dc299
biodata <dimasfebriyanto725@gmail.com>
2023-12-22 14:21:24 +0700

    feat:add arrray methods

---
+++ array-methods/index.js
@@ -0,0 +1,30 @@
+// function bisa di sebut method
+const fruits =['pisang','melon','mangga','aple'];
+
+// Array.push
+fruits.push('strowbery');
+fruits.push('other fruits')
+
+// console.log(fruits.toString())
+
+// latian Array
+const person = ['budi','joni','wahyu'];
+
+// tambah 3 data dengan nama wiliam,erik,thomas
+//tambah kan datab dlm bentuk string
+const push = ['william','erik','thomas']
+// person.push('erik')
+// person.push('thomas')
+// console.log(push.toString())
+
+// array method forEach
+person.forEach(function(data) {
+    console.log(data)
+})
+
+// daily assignment tugas harian
+// buat 5 Array yg  kemuadian akan di tambahkan data dng method push
+// setekah data aray di tambahkan,keluar data dan tampilan dng foreach
+// 4 array dgn nama myhoddie,myfrinends,myFarovouritefood,myLangueage
+// file study-case
+// prefix branch:array-methods
---
+++ array-methods/study-case.js
@@ -0,0 +1,22 @@
+const myhobbie =['sleep','meat'];
+const myfrinends = ['fito','abi','ramzi','andri']
+const myFarovouritefood = ['ayam bakar','sate','nasi goreng']
+const myLanguage = ['php','javascript']
+
+console.log(myhobbie.toString())
+console.log(myfrinends.toString())
+console.log(myFarovouritefood.toString())
+console.log(myLanguage.toString())
+
+myhobbie.forEach(function(data) {
+    console.log(data)
+})
+myfrinends.forEach(function(data) {
+    console.log(data)
+})
+myFarovouritefood.forEach(function(data) {
+    console.log(data)
+})
+myLanguage.forEach(function(data) {
+    console.log(data)
+})
