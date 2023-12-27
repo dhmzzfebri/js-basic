924c07beb30f4e9ce43078436fa57107278ddf5d
Dimas Febriyanto <115466554+dhmzzfebri@users.noreply.github.com>
2023-12-27 14:05:51 +0700

    Update study-case.js
    add times

--- basic-async/study-case.js
+++ basic-async/study-case.js
@@ -1,30 +1,30 @@
 function merebusAir() {
   setTimeout(function () {
     console.log('Merebus air hingga mendidih...');
-  });
+  }.5000);
 }
 
 function memasakMie() {
   setTimeout(function () {
     console.log('Memasak mie hingga matang');
-  });
+  }.3000);
 }
 
 function membuatBumbu() {
   setTimeout(function () {
     console.log('menuangkan bumbu indomei goreng');
-  });
+  }.1000);
 }
 
 function mengandukMie() {
   setTimeout(function () {
     console.log('Mengaduk mie hingga bumbu tercampur');
-  });
+  }.2000);
 }
 function mieDisajikan() {
   setTimeout(function () {
     console.log('Indomie goreng siap di sajikan');
-  });
+  }.1000);
 }
 
 merebusAir()
