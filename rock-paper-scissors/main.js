72a570396232ce207d2522bf04d31bd0e18aa125
Dimas Febriyanto <115466554+dhmzzfebri@users.noreply.github.com>
2023-12-28 10:08:59 +0700

    Update main.js

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -5,23 +5,27 @@ function computerSelect(){
 }
 // console.log(computerSelect());
 
-// function winner(param1,param2){
-//     if(param1 === param2){
-//         return Seri;
-//     }else if(
-//         (param1 === "gunting" && param2 ==="batu" )||
-//         (param1=== "kertas" && param2 ==="gunting") || 
-//         (param1 === "batu" && param2 === "kertas")
-//         ) {
-//             return "pemenang";
-//         } else{
-//             return "computer";
-//         }
-// }
+function winner(userSelect,computerSelect){
+    if(userSelect === computerSelect){
+        return "Seri";
+    }else if(
+        (userSelect === "gunting" && computerSelect ==="batu" )||
+        (userSelect=== "kertas" && computerSelect ==="gunting") || 
+        (userSelect === "batu" && computerSelect === "kertas")
+        ) {
+            return "pemenang";
+        } else{
+            return "computer";
+        }
+}
 
-// function play(main){
-//     let user = main
-//     let computer = computer()
-//     let result = winner(user,computer)
-//     console.log('pemain memilih ${user}')
-// }
+function play(userSelect){
+    const user = userSelect
+    const computer = computerSelect()
+    const result = winner(user,computer)
+    console.log(`pemain memilih ${user}`)
+    console.log(`komputer memilih ${computer}`)
+    console.log(`Hasil ${result}`)
+}
+
+play("kertas");
